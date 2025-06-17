b1dc150166ab9a5e86b568a939c273861e194f52
Naveen Challa <challanaveen03@gmail.com>
2025-06-16 21:13:12 -0700

    added graduation images, formatted them properly

--- src/components/Graduation.js
+++ src/components/Graduation.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import grad1 from '../images/IMG_0733.jpg';
+import grad2 from '../images/uwlogo.jpg';
+import grad3 from '../images/grad3.jpg';
+import grad4 from '../images/grad4.jpg';
 
 const Graduation = () => {
-  return (
-    <div className="celebration">
-      <h1>HAPPY GRADUATION!!! 🎓🎓🎓</h1>
-      <div className="graduation-images">
-        <div className="grad-img placeholder">Graduation Image 1</div>
-        <div className="grad-img placeholder">Graduation Image 2</div>
-        <div className="grad-img placeholder">Graduation Image 3</div>
-      </div>
+  return (    <div className="graduation-container">
+      <h1 className="graduation-header">HAPPY GRADUATION!!! 🎓🎓🎓</h1>
+      <img src={grad1} alt="grad1" className="graduation-image1" />
+      <img src={grad2} alt="grad2" className="graduation-image2" />
+      <img src={grad3} alt="grad3" className="graduation-image3" />
+      <img src={grad4} alt="grad4" className="graduation-image4" />
     </div>
   );
 };
